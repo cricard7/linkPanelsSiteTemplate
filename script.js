c0a595896883b8fc1733c0ea49912971e7d3a571
cricard7 <chad_ricard@hotmail.com>
2017-05-18 00:12:43 -0500

    added printThis.js, updated modal example with a print button that prints the content of the modal

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
-
-
-
 //Initialize all popovers
 
 $(function () {
-  $('[data-toggle="popover"]').popover()
+    $('[data-toggle="popover"]').popover()
 })
 
+
+$(function () {
+
+
+    $("#modalPrint").click(function () {
+
+        $("#modalDivPrint").printThis({
+            debug: false,
+            importCSS: true,
+            importStyle: true,
+            printContainer: true,
+            loadCSS: "../css/style.css",
+            pageTitle: "My Modal",
+            removeInline: false,
+            printDelay: 333,
+            header: null,
+            formValues: true
+        });
+    })
+
+});
